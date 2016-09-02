29173134b7ba5a8ce620a0c7b07b6ce7bc9fbb58
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-02 23:49:52 +0200

    Added travis.

---
+++ build_vrl_ug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ZIP_NAME="natives.zip"
+cd lib/
+zip -r "${ZIP_NAME}" *.so
+mkdir -p vrl_natives/linux/x86
+
+mkdir vrl-ug
+git clone https://github.com/VRL-Studio/VRL-UG
+
+ant clean
+
+git checkout master
+git pull
+ant compile
+
+
