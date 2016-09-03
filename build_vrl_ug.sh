2d6f2df496cd84dbc10ae6bc684371f4a6427fb4
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-03 19:21:04 +0200

    Fix...

--- build_vrl_ug.sh
+++ build_vrl_ug.sh
@@ -20,9 +20,10 @@ cd ../../;
 
 cd VRL-UG/VRL-UG/
 cp /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL/VRL/dist/VRL.jar jars/
-ant clean; ant compile; ant jar
-
+ant clean; ant compile; 
 mkdir -p /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL/VRL/dist/lib/
+ant jar
+
 
 cd ../../;
 mkdir console-app;
