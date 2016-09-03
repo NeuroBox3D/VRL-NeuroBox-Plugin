1c012f3d0c72d12d64b872fdd26763eeb37263b5
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-03 22:49:01 +0200

    Showing file size.

--- build_vrl_ug.sh
+++ build_vrl_ug.sh
@@ -39,6 +39,7 @@ rm -rf .application/property-folder/plugins/unzipped/VRL-UG*;
 rm -rf .application/property-folder/plugins/VRL-UG*;
 cp /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/dist-single/temp_final.jar .application/property-folder/plugin-updates/VRL-UG.jar
 jar tf /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/dist-single/temp_final.jar
+du -sh /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/dist-single/temp_final.jar$
 
 # install vrl-ug
 ./run.sh; 
