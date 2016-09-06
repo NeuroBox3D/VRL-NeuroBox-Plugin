ef66e976e2ba67d3bd62359a4fbbe2387c9387cc
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-06 02:29:28 +0200

    Fix.

--- build_vrl_userdata.sh
+++ build_vrl_userdata.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/;
-ls -l final-jars/
 git clone https://github.com/VRL-Studio/VRL-UserData
 cd VRL-UserData;
-cd /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/final-jars/* jars/;
+cp /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/final-jars/* jars/;
+ls -l /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/final-jars/;
 ant clean
 ant compile
 ant jar
