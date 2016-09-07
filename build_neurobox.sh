da6dd50bf6f75e1ee3fe09be60f32b28b01e0492
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-07 03:33:10 +0200

    Show build.properties in VRL-NeuroBox-Plugin (seems to be wrong path).

--- build_neurobox.sh
+++ build_neurobox.sh
@@ -2,5 +2,7 @@
 
 cd /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/;
 sed -i.bak 's/\(vrldir\).*/\1=\/home\/travis\/build\/NeuroBox3D\/final-jars\//' build.properties
+cat build.properties
+ls -l /home/travis/build/NeuroBox3D/final-jars/
 ./gradlew build
 # [[ $? -eq 0 ]] && ./gradlew installVRLPlugin
