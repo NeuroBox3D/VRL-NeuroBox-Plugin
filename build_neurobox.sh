ab7d8cb6563e67c0f4a5fb73732ce4b4b50c20c2
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-07 16:53:06 +0200

    Adding jars for VRL-NeuroBox-Plugin dependencies.

--- build_neurobox.sh
+++ build_neurobox.sh
@@ -3,6 +3,8 @@
 cd /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/;
 sed -i.bak 's/\(vrldir\).*/\1=\/home\/travis\/build\/NeuroBox3D\/final-jars\//' build.properties
 cat build.properties
+mkdir lib/;
+cp /home/travis/build/NeuroBox3D/final-jars/* lib/;
 ls -l /home/travis/build/NeuroBox3D/final-jars/
 ./gradlew build
 # [[ $? -eq 0 ]] && ./gradlew installVRLPlugin
