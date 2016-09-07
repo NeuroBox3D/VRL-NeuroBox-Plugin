c46398c5fd73ad6565b3505c61b357d7dfeb9b56
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-07 19:55:23 +0200

    Updated.

---
+++ build_neurolucida.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /home/travis/build/NeuroBox3D/VRL-Neurolucida-Project/;
+git clone https://github.com/stephanmg/VRL-Neurolucida-Project
+mkdir lib/;
+cp /home/travis/build/NeuroBox3D/final-jars/* lib/;
+./gradlew build
+# [[ $? -eq 0 ]] && ./gradlew installVRLPlugin
