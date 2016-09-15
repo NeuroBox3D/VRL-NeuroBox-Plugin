0c53f0e14eb5ce7ff0012e7d57dde1a222f0e79f
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-15 20:01:53 +0200

    Try to find correct application dir.

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-cd C:\projects\vrl-neurobox-plugin\lib\console-app\ugInit-consolApp\.application\
+cd C:\projects\vrl-neurobox-plugin\lib\ugInit-consolApp\.application
 echo "Now in application dir:"
 dir
 java -Xms128m -Xmx4096m -XX:MaxPermSize=256m -Djava.library.path="$LIBDIR:\home\travis\build\NeuroBox3D\VRL-NeuroBox-Plugin\lib\VRL-UG\VRL-UG\jars\apache-xmlrpc-3.1.3\lib\commons-logging-1.1.jar:\home\travis\build\NeuroBox3D\VRL-NeuroBox-Plugin\lib\VRL-UG\VRL-UG\jars\apache-xmlrpc-3.1.3\lib\ws-commons-util-1.0.2.jar:\home\travis\build\NeuroBox3D\VRL-NeuroBox-Plugin\lib\VRL-UG\VRL-UG\jars\apache-xmlrpc-3.1.3\lib\xmlrpc-client-3.1.3.jar:\home\travis\build\NeuroBox3D\VRL-NeuroBox-Plugin\lib\VRL-UG\VRL-UG\jars\apache-xmlrpc-3.1.3\lib\xmlrpc-common-3.1.3.jar:\home\travis\build\NeuroBox3D\VRL-NeuroBox-Plugin\lib\VRL-UG\VRL-UG\jars\apache-xmlrpc-3.1.3\lib\xmlrpc-server-3.1.3.jar"  -jar project.jar -property-folder property-folder\ -plugin-checksum-test no -install-plugin-help no -install-payload-plugins yes
