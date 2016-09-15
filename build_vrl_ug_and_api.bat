e4fa924bdc5d59bd1a27ffd9238260e4ba8bfcb1
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-15 04:24:14 +0200

    Try run Win Build.

--- build_vrl_ug_and_api.bat
+++ build_vrl_ug_and_api.bat
@@ -38,95 +38,19 @@ xcopy C:\projects\vrl-neurobox-plugin\lib\VRL\VRL\dist\VRL.jar ugInit-consolApp\
 xcopy C:\projects\vrl-neurobox-plugin\lib\VRL-UG\VRL-UG\dist\VRL-UG.jar ugInit-consolApp\.application\property-folder\plugin-updates\
 xcopy C:\projects\vrl-neurobox-plugin\lib\VRL-UG\VRL-UG\dist\VRL-UG.jar ugInit-consolApp\.application\property-folder\plugins\
 
-echo "DEBUG FROM HERE"
-
-# install vrl-ug plugin
+echo "DEBUG FROM HERE 1"
 call run.bat
-# build vrl-ug-api
 call run.bat
-# test
+set BASEPATH=C:\projects\vrl-neurobox-plugin\lib\VRL-UG\VRL-UG\jars
 
-echo "DEBUG FROM HERE"
-
-BASEPATH=/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars
-
-cp $BASEPATH/apache-xmlrpc-3.1.3/lib/commons-logging-1.1.jar .application/lib/
+xcopy %BASEPATH%\apache-xmlrpc-3.1.3\lib\commons-logging-1.1.jar ugInit-consolApp\.application\lib\
+echo "DEBUG FROM HERE 2"
 cp $BASEPATH/apache-xmlrpc-3.1.3/lib/ws-commons-util-1.0.2.jar .application/lib/
 cp $BASEPATH/apache-xmlrpc-3.1.3/lib/xmlrpc-client-3.1.3.jar .application/lib
 cp $BASEPATH/apache-xmlrpc-3.1.3/lib/xmlrpc-common-3.1.3.jar .application/lib
 cp $BASEPATH/apache-xmlrpc-3.1.3/lib/xmlrpc-server-3.1.3.jar .application/lib
 
-echo "TEST TEST";
-
-CALL_PATH=$(pwd)
-APPDIR="$(dirname "$0")/.application"
-cd "$APPDIR"
-APPDIR="$(pwd)"
-
-PROPERTY_FOLDER="property-folder/"
-PROJECT_FILE="project.jar"
-
-CONF="-property-folder $PROPERTY_FOLDER -plugin-checksum-test no -install-plugin-help no -install-payload-plugins yes"
-
-OS=$(uname -a)
-
-
-if [ "$1" == "-help" ]
-then
-
-cat << EOF
-
-** Help: **
-
-Basic Usage:  
-
-./run.sh                      Starts VRL-Project
-
-EOF
-
-exit 0
-
-fi
-
-LIBDIR32="lib/linux/x86:custom-lib/linux/x86"
-LIBDIR64="lib/linux/x64:custom-lib/linux/x64"
-LIBDIROSX="lib/osx:custom-lib/osx"
-
-if [[ $OS == *x86_64* ]]
-then
-  echo ">> detected x86 (64 bit) os"
-  LIBDIR="$LIBDIR64:$LIBDIROSX"
-  JAVAEXE="jre/x64/bin/java"
-elif [[ $OS == *86* ]]
-then
-  echo ">> detected x86 (32 bit) os"
-  LIBDIR="$LIBDIR32:$LIBDIROSX"
-  JAVAEXE="jre/x86/bin/java"
-else
-  echo ">> unsupported architecture!"
-  echo " --> executing installed java version"
-  JAVAEXE="java"
-fi
-
-if [ ! -e $JAVAEXE ]
-then
-  echo ">> integrated jre not found!"
-  echo " --> executing installed java version"
-  JAVAEXE="java"
-fi
-
-if [[ $OS == *Darwin* ]]
-then
-  # ugly hack to enable vtk on osx
-  export DYLD_LIBRARY_PATH="$PROPERTY_FOLDER/plugins/VRL-VTK/natives/osx/:$DYLD_LIBRARY_PATH"
-  # force java 6 on mac os
-  JAVAEXE=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin/java
-fi
-  
-# optimized for jre 7 (19.04.2012)
-$JAVAEXE -Xms128m -Xmx4096m -XX:MaxPermSize=256m -Djava.library.path="$LIBDIR:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/commons-logging-1.1.jar:/Users/markus/Developing/VRL/VRL/VRL/jars/groovy/groovy-all.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/ws-commons-util-1.0.2.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/xmlrpc-client-3.1.3.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/xmlrpc-common-3.1.3.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/xmlrpc-server-3.1.3.jar"  -jar "$PROJECT_FILE" $CONF
-
-$JAVAEXE -Xms128m -Xmx4096m -XX:MaxPermSize=256m -Djava.library.path="$LIBDIR:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/commons-logging-1.1.jar:/Users/markus/Developing/VRL/VRL/VRL/jars/groovy/groovy-all.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/ws-commons-util-1.0.2.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/xmlrpc-client-3.1.3.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/xmlrpc-common-3.1.3.jar:/home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/VRL-UG/VRL-UG/jars/apache-xmlrpc-3.1.3/lib/xmlrpc-server-3.1.3.jar"  -jar "$PROJECT_FILE" $CONF
+call run.bat
 
 echo "VRL-UG-API information:"
 file /home/travis/build/NeuroBox3D/VRL-NeuroBox-Plugin/lib/console-app/ugInit-consolApp/.application/property-folder/plugins/VRL-UG-API.jar
