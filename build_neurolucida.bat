b5b642cd1ae010c495b16d829c8476c46b413783
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-17 20:48:23 +0200

    Building VRL-Neurolucida on Windows.

--- build_neurolucida.bat
+++ build_neurolucida.bat
@@ -1,1 +1,8 @@
-echo "TODO"
+git clone https://github.com/stephanmg/VRL-Neurolucida-Project
+cd VRL-Neurolucida-Project;
+mkdir lib/;
+xcopy C:\projects\vrl-neurobox-plugin\lib\VRL\VRL\dist\VRL.jar lib\
+xcopy C:\projects\vrl-neurobox-plugin\lib\VRL-UG\VRL-UG\dist\VRL-UG.jar lib\
+xcopy C:\projects\vrl-neurobox-plugin\lib\ugInit-consolApp\.application\property-folder\plugins\VRL-UG-API.jar lib\
+./gradlew.bat build
+./gradlew.bat jar
--- build_vrl_userdata.bat
+++ build_vrl_userdata.bat
@@ -3,6 +3,7 @@ cd VRL-UserData
 xcopy C:\projects\vrl-neurobox-plugin\lib\VRL\VRL\dist\VRL.jar jars\
 xcopy C:\projects\vrl-neurobox-plugin\lib\VRL-UG\VRL-UG\dist\VRL-UG.jar jars\
 xcopy C:\projects\vrl-neurobox-plugin\lib\ugInit-consolApp\.application\property-folder\plugins\VRL-UG-API.jar jars\
+xcopy C:\projects\vrl-neurobox-plugin\final-jars\VRL-UserData.jar jars\
 call ant clean
 call ant compile
 call ant jar
