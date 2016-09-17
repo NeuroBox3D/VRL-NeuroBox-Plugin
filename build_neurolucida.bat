24d434c8c9824d01a4613a3d30fbe782206de26e
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-17 21:53:20 +0200

    Fix.

--- build_neurolucida.bat
+++ build_neurolucida.bat
@@ -1,8 +1,8 @@
 git clone https://github.com/stephanmg/VRL-Neurolucida-Project
-cd VRL-Neurolucida-Project;
-mkdir lib/;
+cd VRL-Neurolucida-Project
+mkdir lib\
 xcopy C:\projects\vrl-neurobox-plugin\lib\VRL\VRL\dist\VRL.jar lib\
 xcopy C:\projects\vrl-neurobox-plugin\lib\VRL-UG\VRL-UG\dist\VRL-UG.jar lib\
 xcopy C:\projects\vrl-neurobox-plugin\lib\ugInit-consolApp\.application\property-folder\plugins\VRL-UG-API.jar lib\
-./gradlew.bat build
-./gradlew.bat jar
+CALL gradlew.bat build
+CALL gradlew.bat jar
