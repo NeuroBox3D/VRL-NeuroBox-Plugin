42fb16b27dbe8ccce8c704ddbc24c85fb15b00a1
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-17 18:06:58 +0200

    Fix.

--- build_vrl_userdata.bat
+++ build_vrl_userdata.bat
@@ -1,8 +1,8 @@
 git clone https://github.com/VRL-Studio/VRL-UserData
 cd VRL-UserData
-xcopy C:\projects\vrl-neurobox-plugin\final-jars\VRL-UG.jar jars\
-xcopy C:\projects\vrl-neurobox-plugin\final-jars\VRL-UG-API.jar jars\
-xcopy C:\projects\vrl-neurobox-plugin\final-jars\VRL.jar jars\
+xcopy C:\projects\vrl-neurobox-plugin\lib\VRL\VRL\dist\VRL.jar \jars
+xcopy C:\projects\vrl-neurobox-plugin\lib\VRL-UG\VRL-UG\dist\VRL-UG.jar jars\
+xcopy C:\projects\vrl-neurobox-plugin\lib\ugInit-consolApp\.application\property-folder\plugins\VRL-UG-API.jar \jars
 call ant clean
 call ant compile
 call ant jar
