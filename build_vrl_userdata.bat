a1cb641c6c8d9e8b342897338bae1000d0bf4353
stephanmg <stephan@syntaktischer-zucker.de>
2016-09-17 16:38:58 +0200

    Fix for VRL-Userdata build on Windows.

--- build_vrl_userdata.bat
+++ build_vrl_userdata.bat
@@ -1,7 +1,9 @@
 git clone https://github.com/VRL-Studio/VRL-UserData
 cd VRL-UserData
-xcopy C:\projects\vrl-neurobox-plugin\final-jars\* %cd%
-ant clean
-ant compile
-ant jar
+xcopy C:\projects\vrl-neurobox-plugin\final-jars\VRL-UG.jar jars\
+xcopy C:\projects\vrl-neurobox-plugin\final-jars\VRL-UG-API.jar jars\
+xcopy C:\projects\vrl-neurobox-plugin\final-jars\VRL.jar jars\
+call ant clean
+call ant compile
+call ant jar
 xcopy dist\VRL-UserData.jar C:\projects\vrl-neurobox-plugin\final-jars\
